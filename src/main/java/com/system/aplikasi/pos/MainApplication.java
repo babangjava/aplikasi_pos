335e57ff18a7bca885bb66581b10b49a1c11b2fd
Ade Kenrian <80423636+babangjava@users.noreply.github.com>
2024-06-25 23:31:22 +0700

    Update MainApplication.java
    update cors

--- src/main/java/com/system/aplikasi/pos/MainApplication.java
+++ src/main/java/com/system/aplikasi/pos/MainApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://aplikasipos-production.up.railway.app", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "https://aplikasipos-production-69fe.up.railway.app", description = "Default Server URL")})
 @SpringBootApplication
 public class MainApplication {
 
